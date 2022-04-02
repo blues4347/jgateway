baf0255e39f76c3b36a524b8f17886cf4aacfbad
李军锋 <lijunfeng@5i5j.com>
2022-04-02 14:46:00 +0800

    GW-5，请求留痕

--- src/main/java/me/lijf/jgateway/filter/JGlobalLoggingFilter.java
+++ src/main/java/me/lijf/jgateway/filter/JGlobalLoggingFilter.java
@@ -1,42 +1,28 @@
 package me.lijf.jgateway.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.nio.charset.StandardCharsets;
-
-//@Component
+@Slf4j
+@Component
 public class JGlobalLoggingFilter implements GlobalFilter, Ordered {
-    private Logger logger= LoggerFactory.getLogger(JGlobalLoggingFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        String appkey=exchange.getRequest().getQueryParams().getFirst("appkey");
-        if(StringUtils.isEmpty(appkey)){
-            logger.warn("Annoymous@{} attempts to access {}",exchange.getRequest().getRemoteAddress().getHostString(),exchange.getRequest().getPath());
-            ServerHttpResponse response=exchange.getResponse();
-            response.setStatusCode(HttpStatus.NON_AUTHORITATIVE_INFORMATION);
-            String text="No appkey provided.";
-            DataBuffer msg=response.bufferFactory().wrap(text.getBytes(StandardCharsets.UTF_8));
-            return response.writeWith(Mono.just(msg));
-        }
-        logger.info("{}@{} attempts to access {}",appkey,exchange.getRequest().getRemoteAddress().getHostString(),exchange.getRequest().getPath());
-        //继续执行其他filter
+        String appkey=exchange.getRequest().getHeaders().getFirst("5i5j-appkey");
+        log.info("{}@{} requests {}",appkey,exchange.getRequest().getRemoteAddress().getHostString(),exchange.getRequest().getPath());
         return chain.filter(exchange);
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return -9000;
     }
 }
